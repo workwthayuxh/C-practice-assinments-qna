e2e2fd842f923288f4c7b4b8f27981d7f50a68e8
Ayush <workwthayush@gmail.com>
2024-10-20 13:56:22 +0530

    Exeperiment 1 done

---
+++ exp_02.c
@@ -0,0 +1,9 @@
+//WAP that calculates the Simple Interest and Compound Interest. The Principal, Amount, Rate of Interest and time are entered through the keyboard.
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main(){
+
+    getch();
+}
