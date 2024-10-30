3aa30d5266709e871cc11ca8d8a8e3409da27ec1
Ayush <workwthayush@gmail.com>
2024-10-30 20:39:49 +0530

    this folder will be for practice

--- exp_02.c
+++
@@ -1,9 +0,0 @@
-//WAP that calculates the Simple Interest and Compound Interest. The Principal, Amount, Rate of Interest and time are entered through the keyboard.
-
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-void main(){
-    ayushn
-    getch();
-}
